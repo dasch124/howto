638a8e3a23cc2c536b12586a8c0c859f080b1762
Stefan Probst <stefanprobst@posteo.de>
2022-07-12 19:49:52 +0200

    chore: wup

--- stories/preview.tsx
+++ stories/preview.tsx
@@ -3,6 +3,7 @@ import '@/styles/index.css'
 import '@/styles/nprogress.css'
 import '~/stories/preview.css'
 
+import { I18nProvider } from '@stefanprobst/next-i18n'
 import { action } from '@storybook/addon-actions'
 import type { DecoratorFn, Parameters } from '@storybook/react'
 import {
@@ -12,11 +13,12 @@ import {
 import { RouterContext } from 'next/dist/shared/lib/router-context'
 import type { NextRouter } from 'next/router'
 
+import { dictionary as common } from '@/app/i18n/common/en'
 import { createMockRouter } from '@/mocks/create-mock-router'
 
 initializeMockServiceWorker({ onUnhandledRequest: 'bypass' })
 
-const withRouter: DecoratorFn = function withRouter(Story, context) {
+const withRouter: DecoratorFn = function withRouter(story, context) {
   const partial = context.parameters['router'] as Partial<NextRouter>
   const mockRouter = createMockRouter({
     // @ts-expect-error Should return `Promise`.
@@ -26,14 +28,20 @@ const withRouter: DecoratorFn = function withRouter(Story, context) {
     ...partial,
   })
 
-  return (
-    <RouterContext.Provider value={mockRouter}>
-      <Story {...context} />
-    </RouterContext.Provider>
-  )
+  return <RouterContext.Provider value={mockRouter}>{story(context)}</RouterContext.Provider>
 }
 
-export const decorators: Array<DecoratorFn> = [withRouter, withMockServiceWorker as DecoratorFn]
+const withProviders: DecoratorFn = function withProviders(story, context) {
+  const dictionaries = { common }
+
+  return <I18nProvider dictionaries={dictionaries}>{story(context)}</I18nProvider>
+}
+
+export const decorators: Array<DecoratorFn> = [
+  withProviders,
+  withRouter,
+  withMockServiceWorker as DecoratorFn,
+]
 
 export const parameters: Parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
